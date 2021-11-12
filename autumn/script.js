1d2420856dd7684b78b8d0e3bdcc53a4b9bbc034
bestgenshin <78467715+bestgenshin@users.noreply.github.com>
2021-11-12 10:15:06 +0600

    Add files via upload

---
+++ autumn/script.js
@@ -0,0 +1,111 @@
+//https://github.com/VincentGarreau/particles.js/
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 5,
+        "density": {
+          "enable": true,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 3,
+          "color": "#fff"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "./media/autumnv1.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.7,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 20,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 20,
+          "size_min": 10,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 50,
+        "color": "#ffffff",
+        "opacity": 0.6,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "bottom",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 10,
+          "rotateY": 100
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode":  "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "bubble"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 200,
+          "line_linked": {
+            "opacity": 10
+          }
+        },
+        "bubble": {
+          "distance": 100,
+          "size": 30,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 50
+        },
+        "repulse": {
+          "distance": 100,
+          "duration": 1
+        },
+        "push": {
+          "particles_nb": 1
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
